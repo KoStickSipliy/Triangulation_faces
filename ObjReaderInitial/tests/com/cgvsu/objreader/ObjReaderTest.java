84250448b745a0fcb141769c07907cc1203e4443
KoStickSipliy <esipov.kostya.20005@gmail.com>
2024-11-26 18:06:48 +0300

    added a test of triangulating face

--- ObjReaderInitial/tests/com/cgvsu/objreader/ObjReaderTest.java
+++ ObjReaderInitial/tests/com/cgvsu/objreader/ObjReaderTest.java
@@ -1,5 +1,6 @@
 package com.cgvsu.objreader;
 
+import com.cgvsu.Triangulation;
 import com.cgvsu.math.Vector3f;
 import com.cgvsu.model.Polygon;
 import org.junit.jupiter.api.Test;
@@ -61,13 +62,59 @@ class ObjReaderTest {
     }
 
     @Test
-    public void testTriangulatePolygon () {
+    public void testTriangulatePolygon1 () {
         Polygon polygon = new Polygon();
         ArrayList<Integer> vertex = new ArrayList<>();
-        for (int i = 0; i < 6; i++) {
+        for (int i = 3; i < 9; i++) {
             vertex.add(i);
         }
-        //polygon.setVertexIndices();
+        polygon.setVertexIndices(vertex);
+        ArrayList<Polygon> faces = Triangulation.triangulatePolygon(polygon);
+        ArrayList<Integer> real = new ArrayList<>();
+        for (int i = 0; i < faces.size(); i++) {
+            real.add(faces.get(i).getVertexIndices().get(0));
+            real.add(faces.get(i).getVertexIndices().get(1));
+            real.add(faces.get(i).getVertexIndices().get(2));
+        }
+
+        ArrayList<Integer> expected = new ArrayList<>();
+        for (int i = 4; i < 8; i++) {
+            expected.add(3);
+            expected.add(i);
+            expected.add(i + 1);
+        }
+        Assertions.assertEquals(expected, real);
+
+//        ArrayList<Polygon> expectedFaces = new ArrayList<>();
+//        for (int i = 4; i < 8; i++) {
+//            ArrayList<Integer> expectedVertex = new ArrayList<>();
+//            Polygon p = new Polygon();
+//            expectedVertex.add(3); expectedVertex.add(i); expectedVertex.add(i + 1);
+//            p.setVertexIndices(expectedVertex);
+//            expectedFaces.add(p);
+//        }
+     }
+
+    @Test
+    public void testTriangulatePolygon () {
+        //создание полигона и его триангулция
+        Polygon polygon = new Polygon();
+        ArrayList<Integer> vertex = new ArrayList<>();
+        for (int i = 3; i < 8; i++) {
+            vertex.add(i);
+        } polygon.setVertexIndices(vertex);
+        ArrayList<Polygon> faces = Triangulation.triangulatePolygon(polygon);
+
+        // создание ожидаемого триангулированного полигона
+        ArrayList<Polygon> expectedFaces = new ArrayList<>();
+        for (int i = 4; i < 8; i++) {
+            ArrayList<Integer> expectedVertex = new ArrayList<>();
+            Polygon p = new Polygon();
+
+            expectedVertex.add(3); expectedVertex.add(i); expectedVertex.add(i + 1);
+            p.setVertexIndices(expectedVertex);
+            expectedFaces.add(p);
+        }
     }
 
     @Test
